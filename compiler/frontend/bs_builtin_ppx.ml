f693afc390d1832224e087e5a14255b0650a429b
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2025-01-08 14:56:01 +0100

    Put async attributes on function at construction time, not with the builtin ppx.

--- compiler/frontend/bs_builtin_ppx.ml
+++ compiler/frontend/bs_builtin_ppx.ml
@@ -111,8 +111,6 @@ let expr_mapper ~async_context ~in_function_def (self : mapper)
     {e with pexp_desc = Pexp_constant (Pconst_integer (s, None))}
   (* End rewriting *)
   | Pexp_newtype (s, body) ->
-    let async = Ast_async.has_async_payload e.pexp_attributes in
-    let body = Ast_async.add_async_attribute ~async body in
     let res = self.expr self body in
     {e with pexp_desc = Pexp_newtype (s, res)}
   | Pexp_fun {arg_label = label; lhs = pat; rhs = body} -> (
--- compiler/ml/ast_async.ml
+++ compiler/ml/ast_async.ml
@@ -2,13 +2,25 @@ let has_async_payload attrs =
   Ext_list.exists attrs (fun ({Location.txt}, _) -> txt = "res.async")
 
 let add_async_attribute ~async (body : Parsetree.expression) =
+  let add (exp : Parsetree.expression) =
+    if has_async_payload exp.pexp_attributes then exp
+    else
+      {
+        exp with
+        pexp_attributes =
+          ({txt = "res.async"; loc = Location.none}, PStr [])
+          :: exp.pexp_attributes;
+      }
+  in
   if async then
-    {
-      body with
-      pexp_attributes =
-        ({txt = "res.async"; loc = Location.none}, PStr [])
-        :: body.pexp_attributes;
-    }
+    let rec add_to_fun (exp : Parsetree.expression) =
+      match exp.pexp_desc with
+      | Pexp_newtype (txt, e) ->
+        {exp with pexp_desc = Pexp_newtype (txt, add_to_fun e)}
+      | Pexp_fun _ -> add exp
+      | _ -> exp
+    in
+    add (add_to_fun body)
   else body
 
 let add_promise_type ?(loc = Location.none) ~async
