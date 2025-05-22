952bdd4512607121cd5eb2df9fc9238c457bccdf
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2025-05-22 08:58:50 +0200

    Missing files in gentype test.
    Some files produced by gentype test were not checked in.

---
+++ tests/gentype_tests/typescript-react-example/src/OnClick2.gen.tsx
@@ -0,0 +1,12 @@
+/* TypeScript file generated from OnClick2.res by genType. */
+
+/* eslint-disable */
+/* tslint:disable */
+
+import * as OnClick2JS from './OnClick2.res.js';
+
+import type {Mouse_t as JsxEvent_Mouse_t} from '../src/shims/JsxEvent.shim';
+
+export type props<onClick> = { readonly onClick: onClick };
+
+export const make: React.ComponentType<{ readonly onClick: (_1:JsxEvent_Mouse_t) => void }> = OnClick2JS.make as any;
