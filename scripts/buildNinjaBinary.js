8e060f2441c3c76498c7357f4fbbe5d84a6840b0
Christoph Knittel <ck@cca.io>
2025-07-04 20:58:42 +0200

    Support python >= 3.11 for ninja build (#7600)

--- scripts/buildNinjaBinary.js
+++ scripts/buildNinjaBinary.js
@@ -6,7 +6,7 @@ import { execSync } from "node:child_process";
 import { ninjaDir } from "#dev/paths";
 
 const platform = process.platform;
-const buildCommand = "python configure.py --bootstrap --verbose";
+const buildCommand = "python3 configure.py --bootstrap --verbose";
 
 if (platform === "win32") {
   // On Windows, the build uses the MSVC compiler which needs to be on the path.
