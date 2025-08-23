cb59f214701cefe72ba1a573644df462fec7b3cf
Florian Hammerschmidt <florianh89@gmail.com>
2025-08-23 08:41:50 +0200

    Set version to 12.0.0-beta.8 (#7793)

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -14,7 +14,7 @@ const execPromise = util.promisify(exec);
  * @param {Yarn.Constraints.Context} ctx
  */
 async function enforceCompilerMeta({ Yarn }) {
-  const EXPECTED_VERSION = "12.0.0-beta.7";
+  const EXPECTED_VERSION = "12.0.0-beta.8";
 
   for (const workspace of Yarn.workspaces()) {
     const { ident } = workspace.pkg;
